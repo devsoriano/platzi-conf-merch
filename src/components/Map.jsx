729fc26399cfb68b131cc38651d9a34679da71f7
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-12-31 13:27:22 -0600

    tienda platzi conf, práctica de hooks

---
+++ src/components/Map.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const Map = ({ data }) => {
+  const mapStyles = {
+    height: '58vh',
+    width: '100%',
+  };
+
+  const defaultCenter = {
+    lat: data.lat,
+    lng: data.lng,
+  };
+
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyA-d2AGRbY5y2hAw82a8tYSzkgazCU9Orw">
+      <GoogleMap mapContainerStyle={mapStyles} zoom={9} center={defaultCenter}>
+        <Marker position={defaultCenter} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Map;
